07af16258b623673d02565988a82ccad83c54b01
krishan <krishans290@gmail.com>
2020-08-06 16:29:42 +0530

    Sum of primes of less than 2 mil.
    first you check whether its prime(check till square root of that number) or not then sum them all

--- Summation of primes.cpp
+++ Summation of primes.cpp
@@ -1,6 +1,32 @@
 #include<iostream>
-#include<ctime>
-#include<math.h>
 using namespace std;
+#include<math.h>
+#include<ctime>
 
 int main()
+{
+    long long int sum=0,j;
+    int count_num=0;
+    for(long int i=2;i<2000000;i++)
+    {
+        j=2;
+        while(j<=sqrt(int(i)))
+        {
+            if(i%j==0)
+            {
+                count_num=1;
+                break;
+            }
+            else
+            {
+                count_num=0;
+            }
+            j++;
+        }
+        if(count_num==0)
+        {
+            sum=sum+i;
+        }
+    }
+    cout<<" answer : "<<sum<<" time : "<<(double)clock();
+}
